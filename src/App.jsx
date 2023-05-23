7b123fff87b9f7105de85d234d8f0e20985920d8
Ramiro <ramiivan59@gmail.com>
2023-05-23 16:39:54 -0300

    mi primer commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Navbar from "./assets/components/layout/Navbar/Navbar";
+import Titulo from "./assets/components/layout/Titulo/Titulo";
+
+const App = () => {
+  return (
+    <div>
+      <Titulo />
+      <Navbar />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/layout/Titulo/Titulo.jsx
@@ -0,0 +1,14 @@
+import "./Titulo.css";
+
+const Titulo = () => {
+  return (
+    <div>
+      <h1>
+        Kat <span className="letra">Y</span>su<span className="letra">R</span>
+        opero
+      </h1>
+    </div>
+  );
+};
+
+export default Titulo;
