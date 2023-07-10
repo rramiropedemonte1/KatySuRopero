f2917e27b2e483f6148882b7bd2a066a57db575f
Ramiro <ramiivan59@gmail.com>
2023-07-10 14:33:48 -0300

    otro commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,22 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./components/layout/Layout";
 import { rutas } from "./routes/rutas";
+import CartContextProvider from "./context/CartContext";
 
 const App = () => {
   return (
-    <div>
-      <BrowserRouter>
-        <Layout />
+    <BrowserRouter>
+      <CartContextProvider>
         <Routes>
-          {rutas.map(({ id, path, Element }) => (
-            <Route key={id} path={path} element={<Element />} />
-          ))}
+          <Route element={<Layout />}>
+            {rutas.map(({ id, path, Element }) => (
+              <Route key={id} path={path} element={<Element />} />
+            ))}
+          </Route>
+          <Route path="*" element={<h1>404 not found</h1>} />
         </Routes>
-      </BrowserRouter>
-    </div>
+      </CartContextProvider>
+    </BrowserRouter>
   );
 };
 
